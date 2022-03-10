01b8b7019dbf047f67681f1a52d19f0066f68c59
Benjamin Gregory Carlisle PhD <bgcarlisle@users.noreply.github.com>
2022-03-10 16:51:50 +0100

    Comment on why I'm using different regexes from Maia's

--- tests/testthat/test-well-formed-trns.R
+++ tests/testthat/test-well-formed-trns.R
@@ -1,12 +1,16 @@
 test_that("All TRN's are well-formed", {
 
-    ## Define well-formed TRN's
+    ## Define well-formed TRN's using very "strict" regexes for QA
+    ## purposes. If you want more "forgiving" regexes for higher
+    ## sensitivity, I recommend the ones documented here:
+    ## https://github.com/maia-sh/ctregistries/
     strict_regexes <- tribble(
         ~registry,            ~regex,
         "ClinicalTrials.gov", "^NCT\\d{8}$",
         "DRKS",               "^DRKS\\d{8}$",
         "EudraCT",            "^\\d{4}-\\d{6}-\\d{2}$"
     )
+    
     ## Combine into a single regex
     combined_regexes <- paste(strict_regexes$regex, collapse="|")
 
